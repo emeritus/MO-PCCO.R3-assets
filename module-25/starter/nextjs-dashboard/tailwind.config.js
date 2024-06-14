44fbe6c9feb636d5a518aa0c22799b8e25ca5724
hitesh.khubchandani@emeritus.org <hitesh.khubchandani@emeritus.org>
2024-06-14 21:57:45 +0530

    Financial Dashboard Starter Code

--- module-25/starter/nextjs-dashboard/tailwind.config.js
+++ module-25/starter/nextjs-dashboard/tailwind.config.js
@@ -2,14 +2,14 @@
 
 const config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
+        13: 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
         blue: {
